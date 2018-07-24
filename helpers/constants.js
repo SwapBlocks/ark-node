1b1964223b2e0e5a72d135fd59a2356fff7139e9
lance <lancekrogers@gmail.com>
2018-07-24 19:38:02 -0400

    updated blockreward start time from 1 week to 2 weeks after network launch

--- helpers/constants.js
+++ helpers/constants.js
@@ -37,7 +37,7 @@ module.exports = {
 			200000000, // Milestone 3
 			200000000  // Milestone 4
 		],
-		offset: 75600,   // Start rewards at block, ie 7 days after net start
+		offset: 151200,   // Start rewards at block, ie 14 days after net start
 		distance: 1500000, // Distance between each milestone
 	},
 	signatureLength: 196,
